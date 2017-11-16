4fa3673051919c2a950fcbd0a5c1647e26e0fd4c
Eugene B <eugenenew80@gmail.com>
2017-11-16 21:33:43 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/dict/AccountingType.java
+++ src/main/java/kz/kegoc/bln/entity/dict/AccountingType.java
@@ -3,15 +3,18 @@ package kz.kegoc.bln.entity.dict;
 import javax.validation.constraints.*;
 import kz.kegoc.bln.entity.common.*;
 import lombok.*;
+import java.util.List;
 
 @Data
 @EqualsAndHashCode(of= {"id"})
 public class AccountingType implements HasId, HasCode, HasName {
 	private Long id;
-	
+
 	@NotNull @Size(max = 10)
 	private String code;
 
 	@NotNull @Size(max = 100)
 	private String name;
+
+	private List<AccountingTypeTl> translations;
 }
