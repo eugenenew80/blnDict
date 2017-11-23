104bd0b64fac4be9893d221e598c6a8913c0e479
Eugene B <eugenenew80@gmail.com>
2017-11-23 20:43:00 +0600

    -

--- src/main/java/kz/kegoc/bln/translator/impl/AccountingTypeTranslatorImpl.java
+++ src/main/java/kz/kegoc/bln/translator/impl/AccountingTypeTranslatorImpl.java
@@ -1,22 +1,21 @@
 package kz.kegoc.bln.translator.impl;
 
 import kz.kegoc.bln.entity.common.Lang;
-import kz.kegoc.bln.entity.dict.MeteringPoint;
-import kz.kegoc.bln.entity.dict.translate.MeteringPointTranslate;
+import kz.kegoc.bln.entity.dict.AccountingType;
+import kz.kegoc.bln.entity.dict.translate.AccountingTypeTranslate;
 import kz.kegoc.bln.translator.Translator;
-
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 @Stateless
-public class AccountingTypeTranslatorImpl implements Translator<MeteringPoint> {
-    public MeteringPoint translate(MeteringPoint entity, Lang lang) {
+public class AccountingTypeTranslatorImpl implements Translator<AccountingType> {
+    public AccountingType translate(AccountingType entity, Lang lang) {
         entity.setLang(lang);
 
         if (entity.getTranslations()==null)
             return entity;
 
-        MeteringPointTranslate translate = entity.getTranslations().get(lang);
+        AccountingTypeTranslate translate = entity.getTranslations().get(lang);
         if (translate==null)
             translate = entity.getTranslations().get(defLang);
 
