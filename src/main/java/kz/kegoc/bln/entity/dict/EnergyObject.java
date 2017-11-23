ce35844d4a0df7182419d500c7e60aa23a31bcee
eugenenew80 <eugenenew80@gmail.com>
2017-11-23 09:03:59 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/dict/EnergyObject.java
+++ src/main/java/kz/kegoc/bln/entity/dict/EnergyObject.java
@@ -1,30 +1,14 @@
 package kz.kegoc.bln.entity.dict;
 
-import kz.kegoc.bln.entity.common.HasCode;
 import kz.kegoc.bln.entity.common.HasName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import javax.persistence.*;
 
 @Data
-@EqualsAndHashCode(of= {"id"})
-@Entity @IdClass(EnergyObjectId.class)
-@Table(name = "dict_energy_objects_v", schema = "apps")
-public class EnergyObject implements HasCode, HasName {
-	@Id
-	@Column(name = "object_id")
+@EqualsAndHashCode(of= {"objectId", "object_type"})
+public class EnergyObject implements HasName {
 	private Long objectId;
-
-	@Id
-	@Column(name = "object_type")
 	private String objectType;
-
-	@Column(name = "code")
-	private String code;
-
-	@Column(name = "name")
 	private String name;
-
-	@Column(name = "short_name")
 	private String shortName;
 }
