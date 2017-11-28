5d0e8eb15a34875d560188060dbff2be405c1d84
Eugene B <eugenenew80@gmail.com>
2017-11-28 22:54:20 +0600

    -

--- src/main/java/kz/kegoc/bln/entity/dict/ContactEmail.java
+++ src/main/java/kz/kegoc/bln/entity/dict/ContactEmail.java
@@ -3,24 +3,26 @@ package kz.kegoc.bln.entity.dict;
 import kz.kegoc.bln.entity.common.HasId;
 import kz.kegoc.bln.entity.common.HasLang;
 import kz.kegoc.bln.entity.common.Lang;
+import kz.kegoc.bln.entity.dict.translate.ContactEmailTranslate;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Map;
 
 @Data
 @EqualsAndHashCode(of= {"id"})
 public class ContactEmail implements HasId, HasLang {
 	private Long id;
 	private Lang lang;
+	private String description;
 
 	@NotNull @Size(max = 100)
 	private String email;
 
-	@Size(max = 100)
-	private String description;
-
 	@NotNull
 	private Contact contact;
+
+	private Map<Lang, ContactEmailTranslate> translations;
 }
--- src/main/java/kz/kegoc/bln/entity/dict/translate/ContactEmailTranslate.java
+++ src/main/java/kz/kegoc/bln/entity/dict/translate/ContactEmailTranslate.java
@@ -2,31 +2,25 @@ package kz.kegoc.bln.entity.dict.translate;
 
 import kz.kegoc.bln.entity.common.HasId;
 import kz.kegoc.bln.entity.common.HasLang;
-import kz.kegoc.bln.entity.common.HasName;
 import kz.kegoc.bln.entity.common.Lang;
-import kz.kegoc.bln.entity.dict.Substation;
+import kz.kegoc.bln.entity.dict.ContactEmail;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @EqualsAndHashCode(of= {"id"})
-public class SubstationTranslate implements HasId, HasName, HasLang {
+public class ContactEmailTranslate implements HasId, HasLang {
     private Long id;
 
     @NotNull
     private Lang lang;
 
     @NotNull
-    private Substation substation;
+    private ContactEmail contactEmail;
 
     @NotNull @Size(max = 100)
-    private String name;
-
-    @NotNull @Size(max = 10)
-    private String shortName;
-
-    @Size(max = 300)
-    private String address;
+    private String description;
 }
