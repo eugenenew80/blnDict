1427274009f458d964b22bf9c254a263d719a34e
Eugene B <eugenenew80@gmail.com>
2017-11-29 06:54:03 +0600

    -

---
+++ src/main/java/kz/kegoc/bln/ejb/converter/jpa/BooleanToStringConverter.java
@@ -0,0 +1,17 @@
+package kz.kegoc.bln.ejb.converter.jpa;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class BooleanToStringConverter implements AttributeConverter<Boolean, String> {
+    @Override
+    public String convertToDatabaseColumn(Boolean value) {
+        return (value != null && value) ? "Y" : "N";
+    }
+
+    @Override
+    public Boolean convertToEntityAttribute(String value) {
+        return "Y".equals(value);
+    }
+}
