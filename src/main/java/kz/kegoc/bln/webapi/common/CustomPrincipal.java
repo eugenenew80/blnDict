007a196418c410d61ccec33d9a2dc6db642e0682
eugenenew80 <eugenenew80@gmail.com>
2017-12-12 16:31:23 +0600

    Merge branch 'master' of https://github.com/eugenenew80/blnDictApi
    # Conflicts:
    #	src/main/resources/liquibase/dev/release-1/changelog-release-1-file-78.xml

--- src/main/java/kz/kegoc/bln/webapi/common/CustomPrincipal.java
+++ src/main/java/kz/kegoc/bln/webapi/common/CustomPrincipal.java
@@ -5,7 +5,6 @@ import kz.kegoc.bln.entity.adm.User;
 import java.security.Principal;
 
 public class CustomPrincipal implements Principal {
-
     public CustomPrincipal(String name, User user) {
         this.user = user;
         this.name = name;
